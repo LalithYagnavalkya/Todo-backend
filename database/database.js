115edc166fa0f351d941a023e889bacd2f753a21
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-03-30 16:00:30 +0530

    fix mongo

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const password = process.env.DATABASE_KEY;
 
-const url = `mongodb+srv://lallu:${password}@cluster0.a7ji1.mongodb.net/test`;
+const url = `mongodb+srv://lallu:bpi3U6pbdgLyhkLa@cluster0.a7ji1.mongodb.net/test`;
 
 const connectDB = async () => {
   try {
